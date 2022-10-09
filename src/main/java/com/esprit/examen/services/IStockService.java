c2d4f65f3de86d38b411ec1ccbcf81a7a95094e1
aymen-jbara <aymen.jb.06@gmail.com>
2022-10-09 15:13:52 +0100

    function interface

--- src/main/java/com/esprit/examen/services/IStockService.java
+++ src/main/java/com/esprit/examen/services/IStockService.java
@@ -5,4 +5,15 @@ import com.esprit.examen.entities.Stock;
 
 public interface IStockService {
 
+	List<Stock> retrieveAllStocks();
+
+	Stock addStock(Stock s);
+
+	void deleteStock(Long id);
+
+	Stock updateStock(Stock u);
+
+	Stock retrieveStock(Long id);
+
+	String retrieveStatusStock();
 }
