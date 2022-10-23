9e7ecafdf8397ef4cefd4d5cfd3916119454805c
nourhenekheriji <nourhene.kheriji@esprit.tn>
2022-10-23 13:09:26 +0100

    test

--- src/main/java/com/esprit/examen/entities/CategorieProduit.java
+++ src/main/java/com/esprit/examen/entities/CategorieProduit.java
@@ -25,6 +25,8 @@ public class CategorieProduit implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
+
+
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCategorieProduit;
 	private String codeCategorie;
