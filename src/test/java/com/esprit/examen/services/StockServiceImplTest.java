73c877960bdbb6c1d68813c2efd9c65bb1f8c7ae
aymen-jbara <aymen.jb.06@gmail.com>
2022-11-01 00:15:26 +0100

    add jacoco

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import com.esprit.examen.repositories.StockRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
@@ -18,6 +19,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
 
+import java.util.List;
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @Slf4j
 @SpringBootTest
@@ -30,7 +34,8 @@ public class StockServiceImplTest {
     StockRepository sto;
 	@InjectMocks
 	StockServiceImpl stockService;
-	
+	Stock s = new Stock("stock test",10,100);
+
 	@Test
 	public void testAddStock() {
 
@@ -44,15 +49,47 @@ public class StockServiceImplTest {
 		assertTrue(savedStock.getQteMin()>0);
 
 		stockService.deleteStock(savedStock.getIdStock());
-		
-	} 
+
+	}
 
 	@Test
 	public void testDeleteStock() {
 		Stock s = new Stock("stock test",30,60);
 		s.setIdStock(Long.valueOf(100));
+		stockService.addStock(s);
 		stockService.deleteStock(s.getIdStock());
 		assertNull(stockService.retrieveStock(s.getIdStock()));
+	}
+
+	@Test
+	public void testRetrieveStock() {
+		Mockito.when(sto.findById(Mockito.anyLong())).thenReturn(Optional.of(s));
+		Stock s1 = stockService.retrieveStock(s.getIdStock());
+		Assertions.assertNotNull(s1);
+	}
+
+
+	@Test
+	public void testAddstock() {
+		Mockito.when(sto.save(s)).thenReturn(s);
+		Stock s1 = stockService.addStock(s);
+		Assertions.assertNotNull(s1);
+
+	}
+
+	@Test
+	public void testUpdatestock() {
+		s.setQteMin(5);
+		Mockito.when(sto.save(s)).thenReturn(s);
+		Stock s1 = stockService.updateStock(s);
+		Assertions.assertEquals(s,s1);
+
+	}
+
+	@Test
+	public void testDeletestock() {
+		stockService.deleteStock(s.getIdStock());
+		Mockito.verify(sto, Mockito.times(1)).deleteById(s.getIdStock());
 	}
 
 }
