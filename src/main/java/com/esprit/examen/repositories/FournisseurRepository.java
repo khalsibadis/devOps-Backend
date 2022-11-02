431c5915134cfbb949198fbbd349a850dddf88c7
dali-art <73824478+dali-art@users.noreply.github.com>
2022-11-02 22:13:49 +0100

    Create FournisseurRepository.java

---
+++ src/main/java/com/esprit/examen/repositories/FournisseurRepository.java
@@ -0,0 +1,12 @@
+package com.esprit.examen.repositories;
+
+import com.esprit.examen.entities.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+
+}
+Footer
+© 2022 GitHub, Inc.
