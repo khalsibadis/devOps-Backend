9f941f10587af47bb07cd6cc36cba541a3cec52f
badis <badis.khalsi@esprit.tn>
2022-11-04 11:15:15 +0100

    add test

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -2,13 +2,13 @@ package com.esprit.examen.services;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
@@ -32,60 +32,61 @@ import lombok.extern.slf4j.Slf4j;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 class FactureServiceImplTest {
-  /*  @Mock //Definition d'un mock
-            FactureRepository fact;
+
+    @Mock
+    FactureRepository factureRepository;
+
     @InjectMocks
-    FactureServiceImpl facts;
-
-    Facture f = Facture.builder().montantFacture(100000).montantRemise(500).build();
-    //List<Facture> list=new ArrayList<Facture>(){
-    //  {
-    //    add(Facture.builder().dateCreationFacture(new Date("11-09-2022")).dateDerniereModificationFacture(new Date("11-09-2022")).montantFacture(500).montantRemise(50).build());
-    //  add(Facture.builder().dateCreationFacture(new Date("11-09-2022")).dateDerniereModificationFacture(new Date("11-09-2022")).montantFacture(600).montantRemise(60).build());
-    //}
-    // };
+    FactureServiceImpl factureService;
 
     @Test
-    void addFacture() {
-        Facture o_add = new Facture();
-        o_add.setMontantFacture(200);
+    public void testRetrieveFacture() {
 
-        Mockito.when(fact.save(ArgumentMatchers.any(Facture.class))).thenReturn(o_add);
+        Facture facture = new Facture(1L, 100, 500, null, null, null, null, null, null);
 
-        Facture o_added = facts.addFacture(o_add);
-        assertEquals(o_add.getMontantFacture(),o_added.getMontantFacture());
-        verify(fact).save(o_add);
-    }
-    @Test
-    void retrieveFacture () {
+        facture.setIdFacture(1L);
 
-        List<Facture> Factures = new ArrayList<>();
-        Factures.add(new Facture());
+        when(factureRepository.findById(1L)).thenReturn(Optional.of(facture));
+        factureService.retrieveFacture(1L);
+        Assertions.assertNotNull(facture);
 
-        Mockito.when(fact.findAll()).thenReturn(Factures);
+        System.out.println(facture);
+        System.out.println(" Retrieve is working correctly...!!");
 
-        List<Facture> expected = facts.retrieveAllFactures();
-        log.info("get ==> " + String.valueOf(Factures));
-        assertEquals(expected, Factures);
-
-
-        verify(fact).findAll();
     }
 
 
-
     @Test
-    void retrieveFactureById() {
+    public void createFacturekTest()
+    {
+        Facture facture2 = new Facture(2L, 100, 500, null, null, null, null, null, null);
+        facture2.setIdFacture(2L);
 
-        Mockito.when(fact.findById(Mockito.anyLong())).thenReturn(Optional.of(f));
-        Facture facture = facts.retrieveFacture((long) 4);
-        assertNotNull(facture);
-        log.info("get ==> " + facture.toString());
-
-        verify(fact).findById(Mockito.anyLong());
+        factureService.addFacture(facture2);
+        verify(factureRepository, times(1)).save(facture2);
+        System.out.println(facture2);
+        System.out.println(" Create is working correctly...!!");
     }
 
-*/
+
+    @Test
+    public void getAllFactureTest()
+    {
+        List<Facture> Facturelist = new ArrayList<Facture>() {
+
+            {
+                add(new Facture(3L, 100, 700, null, null, null, null, null, null));
+                add(new Facture(4L, 200, 800, null, null, null, null, null, null));
+                add(new Facture(5L, 300, 900, null, null, null, null, null, null));
+            }};
+
+
+        when(factureService.retrieveAllFactures()).thenReturn(Facturelist);
+        //test
+        List<Facture> factureList = factureService.retrieveAllFactures();
+        assertEquals(3, factureList.size());
+        System.out.println(" Retrieve all is working correctly...!!");
+    }
 
 
 
