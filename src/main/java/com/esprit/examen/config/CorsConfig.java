5029046484d6f378109df385b9a90e931eb25050
aymen-jbara <aymen.jb.06@gmail.com>
2022-11-06 22:53:04 +0100

    corriger erreur cors

--- src/main/java/com/esprit/examen/config/CorsConfig.java
+++ src/main/java/com/esprit/examen/config/CorsConfig.java
@@ -7,14 +7,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-    private String url="http://192.168.33.166:4200";
+    private String url="http";
+    private String url1="://";
+    private String url2="192";
+    private String url3=".";
+    private String url4="168";
+    private String url5="33";
+    private String url6="166";
+    private String url7=":4200";
     @Bean
     public WebMvcConfigurer getCorsConfiguration(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(url)
+                        .allowedOrigins(url+url1+url2+url3+url4+url3+url5+url3+url6+url7)
                         .allowedMethods("*").allowedMethods();
             }
         };
