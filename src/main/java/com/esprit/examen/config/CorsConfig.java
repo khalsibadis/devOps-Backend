a2fc5b473146b8e39fd55caf73faad0e886d5cb8
aymen-jbara <aymen.jb.06@gmail.com>
2022-11-06 22:32:20 +0100

    corriger erreur cors

--- src/main/java/com/esprit/examen/config/CorsConfig.java
+++ src/main/java/com/esprit/examen/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.33.166:4200")
+                        .allowedOrigins("Access-Control-Allow-Origin")
                         .allowedMethods("*").allowedMethods();
             }
         };
