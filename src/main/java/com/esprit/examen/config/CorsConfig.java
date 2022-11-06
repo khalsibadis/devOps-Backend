75ed5d5e5607df6f10efb0fa54b3236034a011ae
aymen-jbara <aymen.jb.06@gmail.com>
2022-11-06 13:59:10 +0100

    test

--- src/main/java/com/esprit/examen/config/CorsConfig.java
+++ src/main/java/com/esprit/examen/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("Access-Control-Allow-Origin")
+                        .allowedOrigins("http://192.168.33.166:4200")
                         .allowedMethods("*").allowedMethods();
             }
         };
