ae93c56e678c8478a41b9d3a9fe5e5537a98f41b
aymenjerbi <74024103+aymenjerbi@users.noreply.github.com>
2022-11-07 23:15:24 +0100

    version final

--- src/main/java/com/esprit/examen/repositories/ProduitRepository.java
+++ src/main/java/com/esprit/examen/repositories/ProduitRepository.java
@@ -1,7 +1,9 @@
 package com.esprit.examen.repositories;
 
 import java.util.Date;
+import java.util.List;
 
+import com.esprit.examen.services.IProduitService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,21 +11,13 @@ import org.springframework.stereotype.Repository;
 import com.esprit.examen.entities.Produit;
 
 @Repository
-public interface ProduitRepository extends JpaRepository<Produit, Long> {
+public interface  ProduitRepository extends JpaRepository<Produit, Long> {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*@Query("SELECT sum(df.prixTotal) FROM DetailFacture df where df.produit=:produit and df.facture.dateFacture between :startDate"
+    @Query("SELECT sum(df.prixTotal) FROM DetailFacture df where df.produit=:produit and df.facture.dateFacture between :startDate"
 			+ " and :endDate and df.facture.active=true")
+	List<Produit> retrieveAllProduit(@Param("idProduit") Long idProduit);
+
 	public float getRevenuBrutProduit(@Param("produit") Produit produit, @Param("startDate") Date startDate,
-			@Param("endDate") Date endDate);*/
+			@Param("endDate") Date endDate);
+
 }
